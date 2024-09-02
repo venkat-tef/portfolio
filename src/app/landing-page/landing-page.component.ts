693facc8137bc2c3a51f270513cba8c1eed25df9
venkat-tef <kothakotavenkatnarayana@gmail.com>
2024-09-02 20:36:46 +0530

    features added

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.css'
+  styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+  constructor(
+    private renderer: Renderer2,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
+  ngOnInit(): void {
+    // Check if the platform is browser before accessing `document`
+    if (isPlatformBrowser(this.platformId)) {
+      setTimeout(() => {
+        const heroSection = this.renderer.selectRootElement('.hero-section', true);
+        if (heroSection) {
+          this.renderer.addClass(heroSection, 'show');
+        }
+      }, 2000); // 2 seconds delay
+    }
+  }
 }
