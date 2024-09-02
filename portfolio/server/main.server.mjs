b41e543d51f740c5533b0309d1b849562c65109b
venkat-tef <kothakotavenkatnarayana@gmail.com>
2024-09-02 13:36:22 +0530

    Auto-generated commit

--- portfolio/server/main.server.mjs
+++ portfolio/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-H3OUCEUV.mjs";import"./chunk-Z7UMBX4D.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
+import{a}from"./chunk-GERIUCI2.mjs";import"./chunk-Z7UMBX4D.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
