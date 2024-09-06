5fc30cb9d3f0b30f542fa7c2b1c996eff1b8b1ee
venkat-tef <kothakotavenkatnarayana@gmail.com>
2024-09-07 00:48:46 +0530

    hello

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ResumeComponent } from './resume/resume.component';
 import { ProjctsComponent } from './projcts/projcts.component';
 import { ContactComponent } from './contact/contact.component';
+import { SkillsComponent } from './skills/skills.component';
 
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'portfolio/resume', component: ResumeComponent },
   { path: 'portfolio/projects', component: ProjctsComponent },
-  { path: 'portfolio/contact', component: ContactComponent }
+  { path: 'portfolio/contact', component: ContactComponent },
+  { path: 'portfolio/skills', component: SkillsComponent }
 ];
